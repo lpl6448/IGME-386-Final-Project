d885d4a0bd738dab615234c5ed5150225a8eb9db
lpl6448 <lpl6448@rit.edu>
2024-11-22 16:04:09 -0500

    Functional map selector

--- Assets/Scripts/MapConfigure.cs
+++ Assets/Scripts/MapConfigure.cs
@@ -8,6 +8,8 @@ using UnityEngine.Rendering.HighDefinition;
 
 public class MapConfigure : MonoBehaviour
 {
+    public static MapConfigure Instance { get; private set; }
+
     [SerializeField] private ArcGISMapComponent map;
     [SerializeField] private ArcGISLocationComponent cameraLocation;
 
@@ -19,22 +21,25 @@ public class MapConfigure : MonoBehaviour
     [SerializeField] private Texture2D cloudsTex;
     [SerializeField] private Texture2D reflTex;
 
-    private void Start()
+    private Texture2D rclouds;
+    private Texture2D rrain;
+    private Texture2D rrain2;
+
+    public ArcGISSpatialReference MapReference => map.OriginPosition.SpatialReference;
+
+    public void ReconfigureMap(ArcGISPoint originPoint)
     {
-        ArcGISPoint originPoint = map.OriginPosition;
+        map.OriginPosition = originPoint;
         cameraLocation.Position = new ArcGISPoint(originPoint.X, originPoint.Y, 1200);
+        cameraLocation.Rotation = new ArcGISRotation(0, 90, 0);
 
         ArcGISPoint originPointMercator = GeoUtils.ProjectToSpatialReference(originPoint, ArcGISSpatialReference.WebMercator());
         float2 originMercator = (float2)new double2(originPointMercator.X, originPointMercator.Y);
-        
-        Texture2D rclouds = new Texture2D(256, 256, TextureFormat.R8, false, false);
+
         TextureReprojector.ReprojectTexture(cloudsTex, new float2(-14600000, 2600000), new float2(-6800000, 6500000),
             rclouds, originMercator - cloudsExtent, originMercator + cloudsExtent);
         rclouds.Apply();
-        cloudsVolume.profile.TryGet(out VolumetricClouds volumetricClouds);
-        volumetricClouds.cumulusMap.value = rclouds;
-        
-        Texture2D rrain = new Texture2D(256, 256, TextureFormat.RGBA32, false, false);
+
         TextureReprojector.ReprojectTexture(reflTex, new float2(-14600000, 2600000), new float2(-6800000, 6500000),
             rrain, originMercator - rainExtent, originMercator + rainExtent);
         for (int x = 0; x < rrain.width; x++)
@@ -44,9 +49,7 @@ public class MapConfigure : MonoBehaviour
                 rrain.SetPixel(x, y, new Color(1, 1, 1, c.r));
             }
         rrain.Apply();
-        rainFog.SetTexture("_Rain_Texture", rrain);
-        
-        Texture2D rrain2 = new Texture2D(256, 256, TextureFormat.R8, false, false);
+
         TextureReprojector.ReprojectTexture(reflTex, new float2(-14600000, 2600000), new float2(-6800000, 6500000),
             rrain2, originMercator - cloudsExtent, originMercator + cloudsExtent);
         for (int x = 0; x < rrain2.width; x++)
@@ -56,6 +59,36 @@ public class MapConfigure : MonoBehaviour
                 rrain2.SetPixel(x, y, new Color(math.saturate(math.unlerp(0.01f, 0.05f, c.r)), 1, 1, 1));
             }
         rrain2.Apply();
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        cloudsVolume.profile.TryGet(out VolumetricClouds volumetricClouds);
+
+        rclouds = new Texture2D(256, 256, TextureFormat.R8, false, false);
+        rclouds.wrapMode = TextureWrapMode.Clamp;
+        volumetricClouds.cumulusMap.value = rclouds;
+
+        rrain = new Texture2D(256, 256, TextureFormat.RGBA32, false, false);
+        rrain.wrapMode = TextureWrapMode.Clamp;
+        rainFog.SetTexture("_Rain_Texture", rrain);
+
+        rrain2 = new Texture2D(256, 256, TextureFormat.R8, false, false);
+        rrain2.wrapMode = TextureWrapMode.Clamp;
         volumetricClouds.rainMap.value = rrain2;
+    }
+
+    private void OnDestroy()
+    {
+        if (rclouds != null)
+            Destroy(rclouds);
+        if (rrain != null)
+            Destroy(rrain);
+        if (rrain2 != null)
+            Destroy(rrain2);
     }
 }
---
+++ Assets/Scripts/MapSelector.cs
@@ -0,0 +1,68 @@
+using Esri.ArcGISMapsSDK.Utils.GeoCoord;
+using Esri.GameEngine.Geometry;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class MapSelector : MonoBehaviour
+{
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private RectTransform mapBounds;
+    [SerializeField] private RectTransform selectionBox;
+    [SerializeField] private float selectionExtent;
+    [SerializeField] private float2 mapExtentMin;
+    [SerializeField] private float2 mapExtentMax;
+
+    [SerializeField] private RectTransform mapPanel;
+    [SerializeField] private CanvasGroup alphaGroup;
+
+    private bool grow = false;
+    private float growStartTime;
+
+    private void LateUpdate()
+    {
+        if (!grow && RectTransformUtility.ScreenPointToLocalPointInRectangle(mapBounds, Input.mousePosition, canvas.worldCamera, out Vector2 point))
+        {
+            float2 t = Rect.PointToNormalized(mapBounds.rect, point);
+            if (math.all(t > 0) && math.all(t < 1))
+            {
+                float2 projPoint = math.lerp(mapExtentMin, mapExtentMax, t);
+                selectionBox.anchorMin = math.unlerp(mapExtentMin, mapExtentMax, projPoint - selectionExtent);
+                selectionBox.anchorMax = math.unlerp(mapExtentMin, mapExtentMax, projPoint + selectionExtent);
+                selectionBox.sizeDelta = Vector2.zero;
+                selectionBox.gameObject.SetActive(true);
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    grow = true;
+                    growStartTime = Time.time;
+
+                    ArcGISPoint originMercator = new ArcGISPoint(projPoint.x, projPoint.y, ArcGISSpatialReference.WebMercator());
+                    ArcGISPoint originPoint = GeoUtils.ProjectToSpatialReference(originMercator, MapConfigure.Instance.MapReference);
+                    MapConfigure.Instance.ReconfigureMap(originPoint);
+                    
+                    RectTransformUtility.ScreenPointToLocalPointInRectangle(mapPanel, Input.mousePosition, canvas.worldCamera, out Vector2 panelPoint);
+                    mapPanel.pivot = Rect.PointToNormalized(mapPanel.rect, panelPoint);
+                }
+            }
+            else
+                selectionBox.gameObject.SetActive(false);
+        }
+        else
+            selectionBox.gameObject.SetActive(false);
+
+        if (grow)
+        {
+            float elapsed = Time.time - growStartTime;
+            mapPanel.localScale = Vector3.one * math.min(100000, math.exp(8 * elapsed * elapsed * elapsed));
+
+            if (elapsed > 0.5f)
+            {
+                float t = math.saturate((elapsed - 0.5f) / 0.5f);
+                alphaGroup.alpha = 1 - t;
+
+                if (t == 1)
+                    gameObject.SetActive(false);
+            }
+        }
+    }
+}

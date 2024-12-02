59e7d28dd3239e11c10d30f2e7c5a37d63858ffc
lpl6448 <lpl6448@rit.edu>
2024-12-02 18:26:48 -0500

    TIFF import support

--- Assets/Scripts/RasterImporter.cs
+++ Assets/Scripts/RasterImporter.cs
@@ -1,5 +1,8 @@
 using System.IO;
 using UnityEngine;
+using BitMiracle.LibTiff.Classic;
+using System;
+using System.Text;
 
 public class RasterImporter : MonoBehaviour
 {
@@ -7,18 +10,76 @@ public class RasterImporter : MonoBehaviour
 
     [SerializeField] private string reflectivityPath;
     public Texture2D ReflectivityTexture;
+    public Texture2D TestTex;
 
     private Texture2D ImportTexture(string path, TextureFormat format)
     {
-        byte[] data = File.ReadAllBytes(path);
+        Texture2D tex;
+        string ext = Path.GetExtension(path).ToLower();
 
-        Texture2D tex = new Texture2D(1, 1, TextureFormat.ARGB32, false, false);
-        tex.LoadImage(data);
+        if (ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".exr")
+        {
+            byte[] data = File.ReadAllBytes(path);
 
-        Color[] pixels = tex.GetPixels();
-        tex.Reinitialize(tex.width, tex.height, format, false);
-        tex.SetPixels(pixels);
-        tex.Apply();
+            tex = new Texture2D(1, 1, TextureFormat.ARGB32, false, false);
+            tex.LoadImage(data);
+
+            Color[] pixels = tex.GetPixels();
+            tex.Reinitialize(tex.width, tex.height, format, false);
+            tex.SetPixels(pixels);
+            tex.Apply();
+        }
+        else if (ext == ".tif")
+            using (var tif = Tiff.Open(path, "r"))
+            {
+                Tiff.SetErrorHandler(new ThrowTiffErrorHandler());
+                int width = tif.GetField(TiffTag.IMAGEWIDTH)[0].ToInt();
+                int height = tif.GetField(TiffTag.IMAGELENGTH)[0].ToInt();
+                short bitsPerSample = tif.GetField(TiffTag.BITSPERSAMPLE)[0].ToShort();
+                short samplesPerPixel = tif.GetField(TiffTag.SAMPLESPERPIXEL)[0].ToShort();
+                SampleFormat sampleFormat = (SampleFormat)tif.GetField(TiffTag.SAMPLEFORMAT)[0].Value;
+                if (bitsPerSample != 32)
+                    throw new InvalidDataException($"TIFF must have 32 bits per sample, but it has {bitsPerSample}");
+                if (samplesPerPixel != 1)
+                    throw new InvalidDataException($"TIFF must have 1 sample per pixel, but it has {samplesPerPixel}");
+                if (sampleFormat != SampleFormat.IEEEFP)
+                    throw new InvalidDataException($"TIFF must be in floating-point format, but it is in {sampleFormat} format");
+
+                tex = new Texture2D(width, height, format, false, false);
+                Color[] pixels = new Color[width * height];
+                if (!tif.IsTiled())
+                {
+                    byte[] buffer = new byte[tif.ScanlineSize()];
+                    for (int y = 0; y < height; y++)
+                    {
+                        tif.ReadScanline(buffer, y);
+                        for (int x = 0; x < width; x++)
+                            tex.SetPixel(x, y, new Color(BitConverter.ToSingle(buffer, x * sizeof(float)), 0, 0));
+                    }
+                }
+                else
+                {
+                    int tileWidth = tif.GetField(TiffTag.TILEWIDTH)[0].ToInt();
+                    int tileHeight = tif.GetField(TiffTag.TILELENGTH)[0].ToInt();
+                    byte[] buffer = new byte[tif.TileSize()];
+                    for (int ty = 0; ty < height; ty += tileHeight)
+                        for (int tx = 0; tx < width; tx += tileWidth)
+                        {
+                            tif.ReadTile(buffer, 0, tx, ty, 0, 0);
+                            for (int x = 0; x < tileWidth && x + tx < width; x++)
+                                for (int y = 0; y < tileHeight && y + ty < height; y++)
+                                {
+                                    int start = (y * tileWidth + x) * sizeof(float);
+                                    pixels[(height - 1 - y - ty) * width + x + tx] = new Color(BitConverter.ToSingle(buffer, start), 0, 0);
+                                }
+                        }
+
+                }
+                tex.SetPixels(pixels);
+                tex.Apply();
+            }
+        else
+            throw new NotSupportedException($"File type {ext} is not supported");
 
         tex.filterMode = FilterMode.Bilinear;
         tex.wrapMode = TextureWrapMode.Clamp;
@@ -31,5 +92,66 @@ public class RasterImporter : MonoBehaviour
         Instance = this;
         if (File.Exists(reflectivityPath))
             ReflectivityTexture = ImportTexture(reflectivityPath, TextureFormat.R8);
+    }
+}
+
+public class ThrowTiffErrorHandler : TiffErrorHandler
+{
+    public override void ErrorHandler(Tiff tif, string method, string format, params object[] args)
+    {
+        StringBuilder s = new StringBuilder();
+        if (method != null)
+        {
+            s.Append(string.Format("{0}: ", method));
+        }
+
+        s.Append(string.Format(format, args));
+        s.Append("\n");
+
+        throw new Exception(s.ToString());
+    }
+
+    public override void ErrorHandlerExt(Tiff tif, object clientData, string method, string format, params object[] args)
+    {
+        StringBuilder s = new StringBuilder();
+        s.Append(clientData);
+        if (method != null)
+        {
+            s.Append(string.Format("{0}: ", method));
+        }
+
+        s.Append(string.Format(format, args));
+        s.Append("\n");
+
+        throw new Exception(s.ToString());
+    }
+
+    public override void WarningHandler(Tiff tif, string method, string format, params object[] args)
+    {
+        StringBuilder s = new StringBuilder();
+        if (method != null)
+        {
+            s.Append(string.Format("{0}: ", method));
+        }
+
+        s.Append(string.Format(format, args));
+        s.Append("\n");
+
+        Debug.LogWarning(s.ToString());
+    }
+
+    public override void WarningHandlerExt(Tiff tif, object clientData, string method, string format, params object[] args)
+    {
+        StringBuilder s = new StringBuilder();
+        s.Append(clientData);
+        if (method != null)
+        {
+            s.Append(string.Format("{0}: ", method));
+        }
+
+        s.Append(string.Format(format, args));
+        s.Append("\n");
+
+        Debug.LogWarning(s.ToString());
     }
 }

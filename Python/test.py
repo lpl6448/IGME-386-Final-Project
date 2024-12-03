01a0573da3935cc56277c9a4bdfe40d9d66eecba
lpl6448 <lpl6448@rit.edu>
2024-12-03 16:53:26 -0500

    Fix funny test.py code

--- Python/test.py
+++ Python/test.py
@@ -3,7 +3,7 @@ print("Progress 0 Initializing...")
 import arcpy
 import os
 
-raster = r"C:\Users\lukel\Downloads\rap.t00z.awip32f00.grib2"
+raster = r"D:\Profiles\lpl6448\Downloads\rap.t00z.awip32f01.grib2"
 variables = ["LCDC@LCY", "MCDC@MCY", "HCDC@HCY"] # and others eventually
 arcpy.env.overwriteOutput = True
 
@@ -14,7 +14,6 @@ print("Progress 70 Exporting raster layers...")
 for var in variables:
     r = arcpy.MakeMultidimensionalRasterLayer_md(multiraster, variables=var)
     # Probably more Project function stuff here
-    arcpy.CopyRaster_management(r, os.path.abspath("output1.tif"))
-arcpy.CopyRaster_management(arcpy.MakeMultidimensionalRasterLayer_md(multiraster, variables="HCDC@HCY"), os.path.abspath("output3.tif"))
+    arcpy.CopyRaster_management(r, os.path.abspath(f"output{var}.tif"))
 
 print("Progress 100 Done!")

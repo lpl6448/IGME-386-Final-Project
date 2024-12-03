0086387497aab39ce8db56cfeb3b4a1b3353538b
lpl6448 <lpl6448@rit.edu>
2024-12-03 12:15:52 -0500

    GRIB TESTING

--- Python/test.py
+++ Python/test.py
@@ -1,14 +1,22 @@
 import sys
 import time
+import arcpy
+import os
+from osgeo import gdal
 
-print("Hello world!")
-print("This is a test.")
-time.sleep(1)
-print("Progress 10")
-print("Progress 20.56 Baking cookies")
-print("Progress Eating cookies")
-print("Progress 40.6")
-time.sleep(1)
-print("Progress 100 Done with cookies")
-# raise RuntimeError("uh hoh")
-print("Your name is " + sys.argv[1])
+raster = r"D:\Profiles\lpl6448\Downloads\rap.t00z.awip32f01.grib2"
+# arcpy.env.workspace = r"D:\Profiles\lpl6448\Downloads\rap.t00z.awip32f01.grib2"
+arcpy.env.workspace = os.getcwd()
+arcpy.env.overwriteOutput = True
+print("HI")
+# print(arcpy.MakeRasterLayer_management(raster, "test.tif", band_index=1))
+
+r = arcpy.Raster(raster, True)
+arcpy.MakeRasterLayer_management(r, "test", "Variable = LCDC@LCY", band_index=1)
+# arcpy.Raster("test").save(os.path.abspath("test.tif"))
+arcpy.CopyRaster_management("test", os.path.abspath("test.gdb/test2"))
+
+# print(os.path.abspath("test.tif"))
+# rl.save("test.tif")
+# print(len(arcpy.Describe(rl).children))
+# arcpy.Raster(raster).save("test.tif")

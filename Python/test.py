5fa349480420eb3bb48d54e79c63a428cd38fc0d
lpl6448 <lpl6448@rit.edu>
2024-12-05 00:39:20 -0500

    Use dedicated subset function

--- Python/test.py
+++ Python/test.py
@@ -3,17 +3,16 @@ print("Progress 0 Initializing...")
 import arcpy
 import os
 
-raster = r"D:\Profiles\lpl6448\Downloads\rap.t00z.awip32f01.grib2"
+raster = r"C:\Users\lukel\Downloads\rap.t00z.awip32f00.grib2"
 variables = ["LCDC@LCY", "MCDC@MCY", "HCDC@HCY"] # and others eventually
 arcpy.env.overwriteOutput = True
 
-print("Progress 20 Importing raster layer...")
-multiraster = arcpy.MakeMultidimensionalRasterLayer_md(raster, variables=variables)
-
-print("Progress 70 Exporting raster layers...")
+i = 0
 for var in variables:
-    r = arcpy.MakeMultidimensionalRasterLayer_md(multiraster, variables=var)
+    print(f"Progress {i * 100 // len(variables)} Importing {var}...")
+    r = arcpy.md.SubsetMultidimensionalRaster(raster, variables=var) # USE THIS IT'S WAY FASTER
     # Probably more Project function stuff here
     arcpy.CopyRaster_management(r, os.path.abspath(f"output{var}.tif"))
+    i += 1
 
 print("Progress 100 Done!")
